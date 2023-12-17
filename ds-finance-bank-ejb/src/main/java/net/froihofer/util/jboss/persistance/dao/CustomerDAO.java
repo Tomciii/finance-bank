4a71a2da8c90b4a3234abfde58a8f59a8f98c549
csdc24bb_horvatht <116713@fhwn.ac.at>
2023-12-17 22:20:33 +0100

    EJB: Edit Entities

--- ds-finance-bank-ejb/src/main/java/net/froihofer/util/jboss/persistance/dao/CustomerDAO.java
+++ ds-finance-bank-ejb/src/main/java/net/froihofer/util/jboss/persistance/dao/CustomerDAO.java
@@ -1,4 +1,17 @@
 package net.froihofer.util.jboss.persistance.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
 public class CustomerDAO {
+    @PersistenceContext(unitName = "ds-finance-bank-customerunit")
+
+    private EntityManager entityManager;
+
+    public CustomerDAO() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ds-finance-bank-customerunit");
+        entityManager = emf.createEntityManager();
+    }
 }
--- ds-finance-bank-ejb/src/main/java/net/froihofer/util/jboss/persistance/dao/DepotDAO.java
+++ ds-finance-bank-ejb/src/main/java/net/froihofer/util/jboss/persistance/dao/DepotDAO.java
@@ -1,4 +1,18 @@
 package net.froihofer.util.jboss.persistance.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
 public class DepotDAO {
+
+    @PersistenceContext(unitName = "ds-finance-bank-depotunit")
+
+    private EntityManager entityManager;
+
+    public DepotDAO() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ds-finance-bank-depotunit");
+        entityManager = emf.createEntityManager();
+    }
 }
