d1a1b69a8c9f2e91faa788b6c227f4034c807d7a
csdc24bb_horvatht <116713@fhwn.ac.at>
2023-12-21 06:51:15 +0100

    Commons: Edit Commons

--- ds-finance-bank-common/src/main/java/common/dto/StockDTO.java
+++ ds-finance-bank-common/src/main/java/common/dto/StockDTO.java
@@ -22,62 +22,6 @@ public class StockDTO implements Serializable {
         this.symbol = symbol;
     }
 
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public Long getFloatShares() {
-        return floatShares;
-    }
-
-    public void setFloatShares(Long floatShares) {
-        this.floatShares = floatShares;
-    }
-
-    public String getLastTradePrice() {
-        return lastTradePrice;
-    }
-
-    public void setLastTradePrice(String lastTradePrice) {
-        this.lastTradePrice = lastTradePrice;
-    }
-
-    public String getLastTradeTime() {
-        return lastTradeTime;
-    }
-
-    public void setLastTradeTime(String lastTradeTime) {
-        this.lastTradeTime = lastTradeTime;
-    }
-
-    public Long getMarketCapitalization() {
-        return marketCapitalization;
-    }
-
-    public void setMarketCapitalization(Long marketCapitalization) {
-        this.marketCapitalization = marketCapitalization;
-    }
-
-    public String getStockExchange() {
-        return stockExchange;
-    }
-
-    public void setStockExchange(String stockExchange) {
-        this.stockExchange = stockExchange;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
     @Override
     public String toString() {
         return "StockDTO{" +
